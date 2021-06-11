dbb9a5a5f786d883b65c039e718793b93778f835
jieleejie <2260068153@qq.com>
2021-06-11 20:48:32 +0800

    引入三个图形图表

--- src/containers/bar/bar.jsx
+++ src/containers/bar/bar.jsx
@@ -1,11 +1,53 @@
 import React, { PureComponent } from 'react'
+import ReactECharts from 'echarts-for-react';
 
 export default class Bar extends PureComponent {
+
+    state = {
+        source: [
+            ['product', '2015', '2016', '2017'],
+            ['计算机科学与技术', 43.3, 85.8, 93.7],
+            ['软件工程', 83.1, 73.4, 55.1],
+            ['电子通信', 86.4, 65.2, 82.5],
+            ['人工智能', 72.4, 53.9, 39.1]
+        ]
+    }
+
+    getOption = (source) => {
+        return {
+            title: {
+                text: '学科分布',
+                left: 'center'
+            },
+            legend: {
+                top: 'bottom'
+            },
+            tooltip: {},
+            dataset: {
+                source
+            },
+            xAxis: { type: 'category' },
+            yAxis: {},
+            // Declare several bar series, each will be mapped
+            // to a column of dataset.source by default.
+            series: [
+                { type: 'bar' },
+                { type: 'bar' },
+                { type: 'bar' }
+            ]
+        }
+    }
+
+
     render() {
+        let source = [...this.state.source]
         return (
-            <div>
-                Bar
-            </div>
+            <>
+                <ReactECharts
+                    option={this.getOption(source)}
+                    style={{ height: '100%', width: '100%' }}
+                />
+            </>
         )
     }
 }
--- src/containers/pie/pie.jsx
+++ src/containers/pie/pie.jsx
@@ -1,11 +1,65 @@
 import React, { PureComponent } from 'react'
+import ReactECharts from 'echarts-for-react';
 
 export default class Pie extends PureComponent {
+
+    state = {
+        data: [
+            { value: 40, name: '计算机学院' },
+            { value: 38, name: '湘雅公共卫生学院' },
+            { value: 32, name: '粉末冶金研究院' },
+            { value: 30, name: '交通工程学院' },
+            { value: 28, name: '航空航天学院' },
+            { value: 26, name: '体育教研部' },
+            { value: 22, name: '建筑与艺术学院' },
+            { value: 18, name: '材料科学与工程学院' }
+        ]
+    }
+
+    getOption = (pieChartData) => {
+        return {
+            title: {
+                text: '二级学院',
+                textVerticalAlign:'auto',
+                top:'center'
+            },
+            legend: {
+                top: 'top'
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    mark: { show: true },
+                    dataView: { show: true, readOnly: false },
+                    restore: { show: true },
+                    saveAsImage: { show: true }
+                }
+            },
+            series: [
+                {
+                    name: '面积模式',
+                    type: 'pie',
+                    radius: [50, 250],
+                    center: ['50%', '50%'],
+                    roseType: 'area',
+                    itemStyle: {
+                        borderRadius: 8
+                    },
+                    data: pieChartData
+                }
+            ]
+        };
+    }
+
     render() {
+        let pieChartData = [...this.state.data]
         return (
-            <div>
-                pie
-            </div>
+            <>
+                <ReactECharts
+                    option={this.getOption(pieChartData)}
+                    style={{ height: '100%', width: '100%' }}
+                />
+            </>
         )
     }
 }
