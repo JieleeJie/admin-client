94944f05b92e9606ed781542e17f737205828daf
jieleejie <2260068153@qq.com>
2021-06-24 16:24:08 +0800

    首页文字居中

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,7 +3,7 @@ import React, { PureComponent } from 'react'
 export default class Home extends PureComponent {
     render() {
         return (
-            <div style={{'fontSize':'40px'}}>
+            <div style={{'display':'flex','alignItems':'center','justifyContent':'center','height':'100%','fontSize':'40px'}}>
                 中 南 大 学 欢 迎 您
             </div>
         )
